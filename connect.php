a0a99b25a00b535ab3c48b0d7bddffc290dbe874
Francis Jake Roaya <francisjakeroaya@Jakes-MacBook-Air.local>
2025-07-15 19:48:31 +0800

    Add connect.php for database connection

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$host = getenv('localhost');
+$db = getenv('dbattendance');
+$user = getenv('root');
+$pass = getenv('');
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
